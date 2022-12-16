c7168f9588599dd8abadff9a84dd7642292c1b3a
keaguirre <ke.aguirre@duocuc.cl>
2022-12-15 23:33:34 -0300

    Finalmente arreglado

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ pip install -r requirements.txt
 pip install django
 
 python manage.py collectstatic --no-input
+#Falta makemigrations, no lo dejo automatico para que no deje error y examinarlos
 python manage.py migrate
